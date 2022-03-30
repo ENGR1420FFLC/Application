70706a4722c681c5c75f785a7b0ff28e513541e7
Alex Suh <vnh8du@virginia.edu>
2022-03-30 12:55:07 -0400

    Install mongoose

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,2 +1,11 @@
 const FoodDelivery = require('./models/food_delivery')
 const Location = require('./models/location')
+const Have = require('./models/have')
+const Need = require('./models/need')
+
+const my_location = new Location({
+  longitude: 0,
+  latitude: 0,
+  num_people: 0,
+  expiration: new Date()
+})
