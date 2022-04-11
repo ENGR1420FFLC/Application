9cc0322192e0836c3849e047e493b226fc0eae8f
Alex Suh <vnh8du@virginia.edu>
2022-04-11 14:22:35 -0400

    Refactor garbage

--- backend/models/event.js
+++ backend/models/event.js
@@ -1,11 +1,15 @@
 const mongoose = require('./connection');
 
-const eventSchema = new mongoose.Schema({
-  event_name: String,
+const connectionSchema = new mongoose.Schema({
+  name: String,
+  fromId: mongoose.Types.ObjectId,
+  toId: mongoose.Types.ObjectId,
+  description: String,
+  allergenInformation: String,
   rrule: String,
 })
 
-eventSchema.set('toJSON', {
+connectionSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -13,4 +17,4 @@ eventSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Event', eventSchema)
+module.exports = mongoose.model('Connection', connectionSchema)
